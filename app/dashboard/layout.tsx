727a5759d7ee45697f6ec6b15f342c35bbbe05fe
Emmanuel Nasir <emmanuel.nasi@gmail.com>
2026-02-04 01:07:19 -0800

    full changes

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,8 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
-import { Metadata } from "next";
-
+import { Metadata } from 'next';
+ 
 export const metadata: Metadata = {
-  title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
-  },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  title: 'Invoices',
 };
 
 export default function Layout({
